721a09bace2b6bd29c77ace475e4e64b3eea672f
Encrypted <thoughts.encrypted@gmail.com>
2021-11-05 15:23:41 -0500

    updated page to work for either access tokens or auth codes

--- client.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-
-if (urlParams.get('code'))
-    $("#code").val(urlParams.get('code'));
-else
-    $("#code").val(document.location.hash);
