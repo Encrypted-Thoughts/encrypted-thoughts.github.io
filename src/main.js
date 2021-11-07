a5110c4ba3797311fd7f7510e73045802e0bf2f7
Encrypted <thoughts.encrypted@gmail.com>
2021-11-06 23:36:00 -0500

    migrated auth page over to be a vue app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+import './index.css'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faCopy } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+import Auth from './components/Auth.vue'
+import Home from './components/Home.vue'
+
+const NotFoundComponent = { template: '<p>Page not found</p>' }
+
+const routes = {
+  '/': Home,
+  '/auth': Auth
+}
+
+const SimpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname
+  }),
+
+  computed: {
+    CurrentComponent() {
+      return routes[this.currentRoute] || NotFoundComponent
+    }
+  },
+
+  render() {
+    return h(this.CurrentComponent)
+  }
+}
+
+library.add(faCopy);
+
+createApp(SimpleRouter)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount('#app')
