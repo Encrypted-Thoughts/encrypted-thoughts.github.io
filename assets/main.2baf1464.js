22ad726d76bd432842fa65b5c16b3b49af8d144a
Encrypted <thoughts.encrypted@gmail.com>
2021-11-12 18:46:02 -0600

    Updates

---
+++ assets/main.2baf1464.js
@@ -0,0 +1,18 @@
+import{_ as s}from"./plugin-vue_export-helper.f9bdc015.js";import{P as _,o,c as n,a,p as h,b as u,d as t,r as m,e as f,f as v}from"./vendor.702b46c5.js";const g={mounted(){_.highlightAll()}},w=e=>(h("data-v-22a9041a"),e=e(),u(),e),$={id:"code-block",class:"language-json grid justify-items-center place-content-center w-full h-full"},b=a("        "),y=w(()=>t("code",null,`{\r
+    "name": "Encrypted Thoughts",\r
+    "aliases": ["Encrypted", "ET"],\r
+    "status": "Sleep deprived and somewhere between obsessed and apathetic.",\r
+    "location": "nunya byznis",\r
+    "skills": {\r
+        "major": ["C#", "SQL", "Javascript"],\r
+        "minor": ["Python", "Java"]\r
+    },\r
+    "likes": ["twitch", "chatbots", "audio", "d&d"],\r
+    "dislikes": [\r
+        "sunlight", \r
+        "long walks on the beach", \r
+        "people that don't clear excess time off the microwave"\r
+    ],\r
+    "links": ["https://www.twitch.tv/encryptedthoughts"]\r
+}`,-1)),x=a(`\r
+    `),k=[b,y,x];function B(e,r,c,i,d,l){return o(),n("pre",$,k)}var j=s(g,[["render",B],["__scopeId","data-v-22a9041a"]]);const S={components:{Bio:j}},E={class:"h-full bg-gray-800"},I={class:"flex flexwrap h-full"},T={class:"w-full"};function N(e,r,c,i,d,l){const p=m("Bio");return o(),n("div",E,[t("div",I,[t("div",T,[f(p)])])])}var P=s(S,[["render",N]]);v(P).mount("#app");
